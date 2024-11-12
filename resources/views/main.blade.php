c9dcf8ee7e8fb1ef0b46e634c6b9ba7efffee0f1
RifkyCahya1 <rifkyp234@gmail.com>
2024-11-13 03:20:47 +0700

    Initial commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Portofolio</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=lato:100,300,400,700,900" rel="stylesheet" />
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+        <!-- Styles / Scripts -->
+        @vite('resources/css/app.css')
+    </head>
+    
+    <body>
+
+        @include('Layout.navbar')
+        @yield('content')
+
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+        <script src="./js/swiper.js"></script>
+        <script src="./js/swiperHome.js"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'home']);
+Route::get('About', [App\Http\Controllers\HomeController::class, 'about']);
+Route::get('Portofolio', [App\Http\Controllers\HomeController::class, 'portofolio']);
+
+Route::get('U17', [App\Http\Controllers\PortoController::class, 'U17']);
+Route::get('Istopila', [App\Http\Controllers\PortoController::class, 'Istopila']);
+Route::get('GoTrans', [App\Http\Controllers\PortoController::class, 'GoTrans']);
+Route::get('Portfolio', [App\Http\Controllers\PortoController::class, 'Portfolio']);
