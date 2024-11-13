7ebd44a58d9168fbb094ee8aa40959f451ac4744
RifkyCahya1 <rifkyp234@gmail.com>
2024-11-13 18:50:15 +0700

    P

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -28,6 +28,7 @@
         <script src="./js/swiper.js"></script>
         <script src="./js/swiperHome.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+        @vite('resources/js/app.js')
 
     </body>
 </html>
